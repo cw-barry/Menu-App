4d515e8f2fdd9c8ea6b257fa8c601a87f59747aa
cw-barry <barry@clarusway.com>
2022-09-20 20:22:56 -0400

    feat add filter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React, { useState } from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
-import items from './data';
+import data from './data';
+
+// const categories = ['all', 'breakfast', 'lunch', 'shakes'];
+
+const categories = data.reduce(
+  (acc, item) => {
+    if (!acc.includes(item.category)) acc.push(item.category);
+    return acc;
+  },
+  ['all']
+);
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menu, setMenu] = useState(data);
+
+  const handleFilter = (selected) => {
+    if (selected === 'all') setMenu(data);
+    else {
+      const newMenu = data.filter((item) => item.category === selected);
+      setMenu(newMenu);
+    }
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} handleFilter={handleFilter} />
+        <Menu data={menu} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- src/Categories.js
+++ src/Categories.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ categories, handleFilter }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((item, index) => (
+        <button
+          key={index}
+          type="button"
+          className="filter-btn"
+          onClick={() => handleFilter(categories[index])}
+        >
+          {item}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default Categories;
